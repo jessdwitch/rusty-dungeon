d28a7879e543259593728a910b7bfe4ff02f34a9
jessdwitch <me@datawitch.tech>
2021-07-04 20:36:32 -0700

    add turn state

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -8,6 +8,7 @@ pub fn player_input(
     #[resource] map: &Map,
     #[resource] key: &Option<VirtualKeyCode>,
     #[resource] camera: &mut Camera,
+    #[resource] turn_state: &mut TurnState,
 ) {
     if let Some(key) = key {
         let delta = match key {
@@ -15,15 +16,16 @@ pub fn player_input(
             VirtualKeyCode::Right => Point::new(1, 0),
             VirtualKeyCode::Up => Point::new(0, -1),
             VirtualKeyCode::Down => Point::new(0, 1),
-            _ => return
+            VirtualKeyCode::Space => Point::new(0, 0),
+            _ => return,
         };
-        let mut players = <&mut Point>::query()
-            .filter(component::<Player>());
+        let mut players = <&mut Point>::query().filter(component::<Player>());
         players.iter_mut(ecs).for_each(|pos| {
             let destination = *pos + delta;
             if map.can_enter_tile(destination) {
                 *pos = destination;
                 camera.on_player_move(destination);
+                *turn_state = TurnState::PlayerTurn;
             }
         });
     }
